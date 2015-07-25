a2fa0c8a76f5f9bca7e2290c30de207d05f4b50f
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2015-07-25 18:06:09 -0400

    Minor Fixes

---
+++ src/main/java/com/revtekk/nioflex/utils/NewlineType.java
@@ -0,0 +1,8 @@
+package com.revtekk.nioflex.utils;
+
+/**
+ * Created by Mahesh on 7/25/2015.
+ */
+public enum NewlineType
+{
+}
---
+++ src/test/java/com/revtekk/nioflex/ReadlineServer.java
@@ -0,0 +1,8 @@
+package com.revtekk.nioflex;
+
+/**
+ * Created by Mahesh on 7/25/2015.
+ */
+public class ReadlineServer
+{
+}
