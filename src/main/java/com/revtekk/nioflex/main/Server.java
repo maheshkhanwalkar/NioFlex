58ec8023f36af6a0c553613fcbdddf8aed22ad15
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2019-12-12 10:21:41 -0500

    config: add server options container class

--- src/main/java/com/revtekk/nioflex/main/Server.java
+++ src/main/java/com/revtekk/nioflex/main/Server.java
@@ -1,6 +1,19 @@
 package com.revtekk.nioflex.main;
 
+import com.revtekk.nioflex.config.SecurityType;
+import com.revtekk.nioflex.config.ServerOption;
+import com.revtekk.nioflex.config.SocketType;
+
+import java.net.InetAddress;
+
 public class Server
 {
+    private Server() {}
 
+    public static Server build(InetAddress address, int port,
+                               SocketType socket, SecurityType security,
+                               ServerOption... options)
+    {
+        return null;
+    }
 }
