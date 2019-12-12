663d15f118415524c662abe679497b00099fc63f
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2019-12-11 19:20:53 -0500

    cleanup: use lambdas over anonymous classes

--- src/main/java/com/revtekk/nioflex/NIOServer.java
+++ src/main/java/com/revtekk/nioflex/NIOServer.java
@@ -1,21 +1,5 @@
 package com.revtekk.nioflex;
 
-/*
-    Copyright 2015 Mahesh Khanwalkar
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
 import com.revtekk.nioflex.policy.ThreadPolicy;
 import com.revtekk.nioflex.utils.SocketUtil;
 
@@ -173,13 +157,7 @@ public abstract class NIOServer
 
                         if(policy == ThreadPolicy.THREAD_FOR_ACCEPT ||  policy == ThreadPolicy.THREAD_FOR_ALL)
                         {
-                            pool.submit(new Runnable()
-                            {
-                                public void run()
-                                {
-                                    handleAccept(client, key);
-                                }
-                            });
+                            pool.submit(() -> handleAccept(client, key));
                         }
                         else
                         {
@@ -219,7 +197,7 @@ public abstract class NIOServer
 
                 if(SHUTDOWN)
                 {
-                    //Server will shutdown NOW!
+                    // Server will shutdown NOW!
                     pool.shutdown();
 
                     selector.close();
@@ -267,13 +245,7 @@ public abstract class NIOServer
      */
     public final Thread launchThread()
     {
-        Thread t = new Thread(new Runnable()
-        {
-            public void run()
-            {
-                launch();
-            }
-        });
+        Thread t = new Thread(this::launch);
 
         t.start();
         return t;
